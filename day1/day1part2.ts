8737fcc0c1b0c946521b7660f3ca954bc31ce6ab
Spencer Wood <spencercwood@gmail.com>
2025-12-02 13:37:28 -0800

    Add day 1 part 2 solution

---
+++ day1/day1part2.ts
@@ -0,0 +1,38 @@
+import { readFileSync } from "node:fs";
+
+function main(argv: string[]) {
+  const inputFilePath = argv[2];
+  const input = readFileSync(inputFilePath).toString();
+  const lines = input.split("\n");
+
+  let password = 0;
+  let currentPosition = 50;
+
+  for (const line of lines) {
+    const direction = line.charAt(0);
+    const amount = parseInt(line.slice(1));
+
+    for (let i = 0; i < amount; i++) {
+      if (direction === "L") {
+        currentPosition -= 1;
+      } else {
+        currentPosition += 1;
+      }
+      if (currentPosition === -1) {
+        currentPosition = 99;
+      }
+      if (currentPosition === 100) {
+        currentPosition = 0;
+      }
+      if (currentPosition === 0) {
+        password++;
+      }
+    }
+  }
+
+  console.log(password);
+}
+
+if (import.meta.main) {
+  main(process.argv);
+}
