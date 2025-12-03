63b49267092bc4f96e0b2911023e7433618158c6
Spencer Wood <spencercwood@gmail.com>
2025-12-02 22:34:23 -0800

    Add day 3 part 2 solution

---
+++ day3/day3part2.ts
@@ -0,0 +1,36 @@
+import { readFileSync } from "node:fs";
+
+function main(argv: string[]) {
+  const inputFilePath = argv[2];
+  const input = readFileSync(inputFilePath).toString();
+  const banks = input.split("\n");
+  const totalJoltage = banks.reduce((sum, bank) => {
+    return sum + getLargestJoltage(bank);
+  }, 0);
+  console.log(totalJoltage);
+}
+
+function getLargestJoltage(bank: string): number {
+  const joltages = bank.split("").map((value) => parseInt(value));
+  let result = "";
+  let currentIndex = 0;
+  let nextIndex = 0;
+  let highestValue = 0;
+  for (let i = 12; i > 0; i--) {
+    for (let j = currentIndex; j <= joltages.length - i; j++) {
+      if (joltages[j] > highestValue) {
+        highestValue = joltages[j];
+        nextIndex = j + 1;
+      }
+    }
+    result += String(highestValue);
+    highestValue = 0;
+    currentIndex = nextIndex;
+    nextIndex = nextIndex + 1;
+  }
+  return parseInt(result);
+}
+
+if (import.meta.main) {
+  main(process.argv);
+}
